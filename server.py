fab194c281241989184c0a4af007d72b8ae1dce6
Lucas Alves <varela.alves@outlook.com>
2019-09-18 06:41:59 -0300

    Dataset ok

---
+++ server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import LaserScan
+
+def handle_add_two_ints(req):
+    print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
+    return AddTwoIntsResponse(req.a + req.b)
+
+def add_two_ints_server():
+    rospy.init_node('add_two_ints_server')
+    s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
+    print "Ready to add two ints."
+    rospy.spin()
+
+if __name__ == "__main__":
+    add_two_ints_server()
